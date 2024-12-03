c7d820d794143e671d03f322ff9388d92c1930b1
Vinicios L. Biluca <viniciosbiluca.particular@gmail.com>
2024-12-03 08:35:22 -0300

    Adding some Dummy Values

--- queenbee_api/views.py
+++ queenbee_api/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
+import datetime
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
